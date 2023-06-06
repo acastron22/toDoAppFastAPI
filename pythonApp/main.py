b4595cdfcdaea809798e464a4e9d9fbc680d8b7b
acastron22 <arielcastron22@hotmail.com>
2023-06-06 08:14:13 -0300

    aula até erros e retornar por id

--- pythonApp/main.py
+++ pythonApp/main.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, HTTPException, Path
+from starlette import status
 import models
-from database import engine
+from models import ToDos
+from database import engine, SessionLocal
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependecy = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependecy):
+    return db.query(ToDos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependecy, todo_id: int = Path(gt=0)):
+    todo_model = db.query(ToDos).filter(ToDos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found.')
