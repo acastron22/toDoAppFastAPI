5df445b423456150133a3ce812982dc8d213bb97
acastron22 <arielcastron22@hotmail.com>
2023-06-07 22:40:53 -0300

    user table creation

--- pythonApp/models.py
+++ pythonApp/models.py
@@ -1,8 +1,22 @@
 from database import Base
-from sqlalchemy import Column, Integer, Boolean, String
+from sqlalchemy import Column, ForeignKey, Integer, Boolean, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
+# Crio a classe de users
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    firstName = Column(String)
+    lastName = Column(String)
+    hashedPassword = Column(String)
+    isActive = Column(Boolean, default=True)
+    role = Column(String)
 
 
 # aqui que é criado a tabela ToDo e o q terá cada coluna
@@ -14,3 +28,5 @@ class ToDos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    # para o ForeignKey, eu aponto de qual tabela devo estar chamando
+    ownerId = Column(Integer, ForeignKey("users.id"))
