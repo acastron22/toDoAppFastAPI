77f2fee4f78f38e6e8a4cf01a17c620cd5459cbb
acastron22 <arielcastron22@hotmail.com>
2023-06-08 11:19:35 -0300

    salvando usuarios criados no database

--- pythonApp/routers/auth.py
+++ pythonApp/routers/auth.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, APIRouter
+from typing import Annotated
+from fastapi import Depends, APIRouter
+from starlette import status
 from pydantic import BaseModel
 from models import Users
+from sqlalchemy.orm import Session
 # pacote usado para usar o hashpasswords
 from passlib.context import CryptContext
+from database import SessionLocal
 
 router = APIRouter()
 
@@ -17,9 +21,23 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str
 
+    # devemos ter um db dependency, igual ao todos, para poder armazenar as informações de usuário na tabela
 
-@router.post("/auth")
-async def create_user(createUserRequest: CreateUserRequest):
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+                      createUserRequest: CreateUserRequest):
     createUserModel = Users(
         # não posso fazer igual fiz em todos porque aqui eu possuo o password, no models, eu tenho o hashed_password
         # o que ta na esquerda vem do models, o que ta na direita vem da classe q acabei de criar.
@@ -33,4 +51,5 @@ async def create_user(createUserRequest: CreateUserRequest):
         hashedPassword=bcrypt_context.hash(createUserRequest.password),
         isActive=True
     )
-    return createUserModel
+    db.add(createUserModel)
+    db.commit()
