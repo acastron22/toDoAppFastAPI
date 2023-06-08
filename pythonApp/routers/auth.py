3426799ecf011230054c5e04448db0db371b3ab7
acastron22 <arielcastron22@hotmail.com>
2023-06-08 11:06:53 -0300

    implementação do users hash password

--- pythonApp/routers/auth.py
+++ pythonApp/routers/auth.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 from models import Users
+# pacote usado para usar o hashpasswords
+from passlib.context import CryptContext
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 class CreateUserRequest(BaseModel):
@@ -18,15 +22,15 @@ class CreateUserRequest(BaseModel):
 async def create_user(createUserRequest: CreateUserRequest):
     createUserModel = Users(
         # não posso fazer igual fiz em todos porque aqui eu possuo o password, no models, eu tenho o hashed_password
-        ## o que ta na esquerda vem do models, o que ta na direita vem da classe q acabei de criar. 
-        ## Cuidado porque o nome deve ser igual
-        ## tanto para o que vem de models como o q vem da classe
+        # o que ta na esquerda vem do models, o que ta na direita vem da classe q acabei de criar.
+        # Cuidado porque o nome deve ser igual
+        # tanto para o que vem de models como o q vem da classe
         email=createUserRequest.email,
         userName=createUserRequest.username,
         firstName=createUserRequest.firstName,
         lastName=createUserRequest.lastName,
         role=createUserRequest.role,
-        hashedPassword=createUserRequest.password,
+        hashedPassword=bcrypt_context.hash(createUserRequest.password),
         isActive=True
     )
     return createUserModel
